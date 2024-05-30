effa042008693e4628e0ba0656c3cd4a8e9c6947
Ptopic <petartopic2002@gmail.com>
2024-05-30 23:55:39 +0200

    Fast api cors changes

--- yolov5-fastapi/main.py
+++ yolov5-fastapi/main.py
@@ -34,7 +34,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     expose_headers=["*"],
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Authorization", "Content-Type"],
