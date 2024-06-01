8f28e01525b4a9b898c8c99f5f00c15374dce230
Ptopic <petartopic2002@gmail.com>
2024-06-01 14:20:20 +0200

    Fixed stop detection doesnt stop detection imidietly

--- unoai-nextjs/src/app/page.tsx
+++ unoai-nextjs/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import uploadImage from '@api/unoai/unoai';
 import Image from 'next/image';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 
 interface IPrediction {
@@ -12,95 +12,35 @@ interface IPrediction {
 }
 
 const Home = () => {
+	const predictionTimeout = useRef<NodeJS.Timeout | null>(null);
+	const isDetectingRef = useRef<boolean>(false);
+
 	const webcamRef = useRef<Webcam>(null);
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 
 	const [isDetecting, setIsDetecting] = useState(false);
-
 	const [videoWidth, setVideoWidth] = useState(0);
 	const [videoHeight, setVideoHeight] = useState(0);
-
 	const [tempScore, setTempScore] = useState(0);
 	const [score, setScore] = useState(0);
-
 	const [predictions, setPredictions] = useState<IPrediction[]>([]);
 
 	const cardObjects = [
-		{
-			name: '0',
-			points: 0,
-			color: 'red',
-		},
-		{
-			name: '1',
-			points: 1,
-			color: 'red',
-		},
-		{
-			name: '2',
-			points: 2,
-			color: 'red',
-		},
-		{
-			name: '3',
-			points: 3,
-			color: 'red',
-		},
-		{
-			name: '4',
-			points: 4,
-			color: 'red',
-		},
-		{
-			name: '5',
-			points: 5,
-			color: 'red',
-		},
-		{
-			name: '6',
-			points: 6,
-			color: 'red',
-		},
-		{
-			name: '7',
-			points: 7,
-			color: 'red',
-		},
-		{
-			name: '8',
-			points: 8,
-			color: 'red',
-		},
-		{
-			name: '9',
-			points: 9,
-			color: 'red',
-		},
-		{
-			name: '+4',
-			points: 50,
-			color: 'lime',
-		},
-		{
-			name: '+2',
-			points: 20,
-			color: 'lime',
-		},
-		{
-			name: 'Reverse',
-			points: 20,
-			color: 'lime',
-		},
-		{
-			name: 'Skip',
-			points: 20,
-			color: 'lime',
-		},
-		{
-			name: 'Change color',
-			points: 50,
-			color: 'lime',
-		},
+		{ name: '0', points: 0, color: 'red' },
+		{ name: '1', points: 1, color: 'red' },
+		{ name: '2', points: 2, color: 'red' },
+		{ name: '3', points: 3, color: 'red' },
+		{ name: '4', points: 4, color: 'red' },
+		{ name: '5', points: 5, color: 'red' },
+		{ name: '6', points: 6, color: 'red' },
+		{ name: '7', points: 7, color: 'red' },
+		{ name: '8', points: 8, color: 'red' },
+		{ name: '9', points: 9, color: 'red' },
+		{ name: '+4', points: 50, color: 'lime' },
+		{ name: '+2', points: 20, color: 'lime' },
+		{ name: 'Reverse', points: 20, color: 'lime' },
+		{ name: 'Skip', points: 20, color: 'lime' },
+		{ name: 'Change color', points: 50, color: 'lime' },
 	];
 
 	const videoConstraints = {
@@ -109,28 +49,29 @@ const Home = () => {
 		facingMode: 'environment',
 	};
 
-	let predictionTimeout: NodeJS.Timeout;
+	const predictionFunction = async () => {
+		if (!isDetectingRef.current && predictionTimeout.current) {
+			clearTimeout(predictionTimeout.current);
+			return;
+		} else {
+			try {
+				let imageSrc;
+				if (webcamRef.current) {
+					imageSrc = webcamRef.current.getScreenshot();
+				}
 
-	async function predictionFunction() {
-		if (!isDetecting) return;
-		try {
-			let imageSrc;
-			if (webcamRef.current) {
-				imageSrc = webcamRef.current.getScreenshot();
+				const res = await uploadImage(
+					imageSrc ? imageSrc.split(';base64,')[1] : ''
+				);
+				const predictions = res.result;
+				drawBoundingBoxes(predictions);
+			} catch (error) {
+				console.log(error);
 			}
 
-			const res = await uploadImage(
-				imageSrc ? imageSrc.split(';base64,')[1] : ''
-			);
-
-			const predictions = res.result;
-			drawBoundingBoxes(predictions);
-		} catch (error) {
-			console.log(error);
+			predictionTimeout.current = setTimeout(predictionFunction, 150);
 		}
-
-		predictionTimeout = setTimeout(() => predictionFunction(), 150);
-	}
+	};
 
 	function drawBoundingBoxes(predictions: any) {
 		let cnvs;
@@ -142,17 +83,14 @@ const Home = () => {
 		const ctx = cnvs.getContext('2d');
 
 		if (!ctx) return;
-		// Clear previous drawings
 		ctx.clearRect(0, 0, cnvs.width, cnvs.height);
 
-		// Loop through predictions
 		let predictionsArray = [];
 		let curScore = 0;
 		for (let i = 0; i < predictions.length; i++) {
 			const prediction = predictions[i];
 			let { name, confidence, xmin, ymin, xmax, ymax } = prediction;
 
-			// Convert name string to number
 			const predictionObject = {
 				name: cardObjects[+name].name,
 				points: cardObjects[+name].points,
@@ -160,20 +98,17 @@ const Home = () => {
 			};
 
 			curScore += cardObjects[+name].points;
-
 			predictionsArray.push(predictionObject);
 
 			const color = cardObjects[+name].color;
 			name = cardObjects[+name].name;
 
-			// Draw bounding box
 			ctx.beginPath();
 			ctx.rect(xmin, ymin, xmax - xmin, ymax - ymin);
 			ctx.strokeStyle = color;
 			ctx.lineWidth = 3;
 			ctx.stroke();
 
-			// Add label
 			ctx.font = '18px Arial';
 			ctx.fillStyle = color;
 			ctx.fillText(`${name}: ${confidence.toFixed(2)}`, xmin, ymin - 5);
@@ -188,11 +123,27 @@ const Home = () => {
 	};
 
 	useEffect(() => {
-		if (isDetecting) {
+		isDetectingRef.current = isDetecting;
+
+		if (
+			isDetecting &&
+			!predictionTimeout.current &&
+			isDetectingRef.current == isDetecting
+		) {
 			predictionFunction();
 		} else {
-			clearTimeout(predictionTimeout);
+			if (predictionTimeout.current) {
+				clearTimeout(predictionTimeout.current);
+				predictionTimeout.current = null;
+			}
 		}
+
+		return () => {
+			if (predictionTimeout.current) {
+				clearTimeout(predictionTimeout.current);
+				predictionTimeout.current = null;
+			}
+		};
 	}, [isDetecting]);
 
 	useEffect(() => {
@@ -214,18 +165,59 @@ const Home = () => {
 		}
 	}, []);
 
+	const startDetecting = () => {
+		setIsDetecting(true);
+	};
+
+	const stopDetecting = () => {
+		setIsDetecting(false);
+		setTempScore(0);
+		setPredictions([]);
+		canvasRef.current
+			?.getContext('2d')
+			?.clearRect(0, 0, videoWidth, videoHeight);
+
+		if (predictionTimeout.current) {
+			clearTimeout(predictionTimeout.current);
+			predictionTimeout.current = null;
+		}
+
+		setTimeout(() => {
+			if (predictionTimeout.current) {
+				clearTimeout(predictionTimeout.current);
+				predictionTimeout.current = null;
+			}
+
+			setTempScore(0);
+			setPredictions([]);
+			canvasRef.current
+				?.getContext('2d')
+				?.clearRect(0, 0, videoWidth, videoHeight);
+		}, 500);
+	};
+
+	useEffect(() => {
+		if (!isDetecting) {
+			setTempScore(0);
+			setPredictions([]);
+			canvasRef.current
+				?.getContext('2d')
+				?.clearRect(0, 0, videoWidth, videoHeight);
+		}
+	}, [predictionTimeout.current, isDetectingRef.current, isDetecting]);
+
 	return (
 		<main className="flex flex-col gap-4">
 			<div className="h-fit w-full sm:justify-start justify-between flex gap-4 pt-4 px-4">
 				<button
 					className="bg-green500 text-black p-4"
-					onClick={() => setIsDetecting(true)}
+					onClick={() => startDetecting()}
 				>
 					Start detecting
 				</button>
 				<button
 					className="bg-error text-black p-4"
-					onClick={() => setIsDetecting(false)}
+					onClick={() => stopDetecting()}
 				>
 					Stop detecting
 				</button>
